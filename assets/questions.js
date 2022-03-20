ccc15d367a49b05ca5c2ee4a6a83318b50c404b6
Tariq Stanikzai <98728769+Tariqstanikzai@users.noreply.github.com>
2022-03-20 19:40:25 -0400

    Add files via upload

---
+++ assets/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "  10 + 5 + 3 = ?",
+  answer: "18",
+  options: [
+    "16",
+    "20",
+    "18",
+    "22"
+  ]
+},
+  {
+  numb: 2,
+  question: "(30 - 5) + 3 =?",
+  answer: "28",
+  options: [
+    "25",
+    "30",
+    "32",
+    "28"
+  ]
+},
+  {
+  numb: 3,
+  question: "8 x 5 = ?",
+  answer: "40",
+  options: [
+    "35",
+    "30",
+    "45",
+    "40"
+  ]
+},
+  {
+  numb: 4,
+  question: "(20 / 5) x 4 =?",
+  answer: "16",
+  options: [
+    "12",
+    "20",
+    "30",
+    "16"
+  ]
+},
+  {
+  numb: 5,
+  question: "  (10 x 5) / 25 =?",
+  answer: "2",
+  options: [
+    "2",
+    "25",
+    "50",
+    "5"
+  ]
+},
+
+];
